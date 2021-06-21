182f78dbbd0581bbad708aac8b9f17ab381a3928
wp135 <1418574439@qq.com>
2021-06-21 10:08:32 +0800

    test

--- H/zroot/js/base.js
+++ H/zroot/js/base.js
@@ -1,6 +1,6 @@
 var basePeng = {
 	baseURL : "http://127.0.0.1:8080/NodeCtrl",
-	projectUrl : "D:/data/hbuilder/H/zroot",
+	projectUrl : "https://wp135.github.io/work//H/zroot",
 	projectRoot : "D:/data/hbuilder",
 	
 }
